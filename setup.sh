4184e2a43633c87959df9a11483dddc07a84f0e5
Mike Boutin <mike.boutin@gmail.com>
2014-04-02 19:44:22 -0400

    Common git setup and aliases.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+git config --global push.default simple
+
+git config --global alias.cdiff "diff --cached"
+git config --global alias.ci "commit"
+git config --global alias.lag "log --all --graph --pretty=format:'%C(auto)%h%d %Cgreen%an %Cred(%cr) %Creset%s'"
+git config --global alias.lg "log --graph --pretty=format:'%C(auto)%h%d %Cgreen%an %Cred(%cr) %Creset%s'"
+git config --global alias.lp "log -p"
+git config --global alias.st "status"
+
+echo "git config --global credential.helper"
+echo "    wincred"
+echo "    /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring"
