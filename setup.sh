3b7d107bb375e31c9032f9e9d5fd2e3b16fa1971
Mike Boutin <mike.boutin@gmail.com>
2014-05-05 09:13:14 -0400

    Pre-defined the 'changelog' log format so that log aliases may share the definition.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 git config --global fetch.prune true
+git config --global pretty.changelog "format:%C(auto)%h%d %Cgreen%an %Cred(%cr) %Creset%s"
 git config --global pull.rebase preserve
 git config --global push.default simple
 
 git config --global alias.ci "commit"
 git config --global alias.dl "log --patch -1"
 git config --global alias.ds "diff --staged"
-git config --global alias.lag "log --all --graph --pretty=format:'%C(auto)%h%d %Cgreen%an %Cred(%cr) %Creset%s'"
-git config --global alias.lg "log --graph --pretty=format:'%C(auto)%h%d %Cgreen%an %Cred(%cr) %Creset%s'"
+git config --global alias.lag "log --all --graph --pretty=changelog"
+git config --global alias.lg "log --graph --pretty=changelog"
 git config --global alias.lp "log -p"
 git config --global alias.st "status"
 git config --global alias.unstage "reset HEAD --"
