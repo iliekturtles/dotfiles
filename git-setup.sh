5d66237413b3444e4cf2c747e465a5102fcbc24a
Mike Boutin <mike.boutin@gmail.com>
2016-06-14 14:23:12 -0400

    Change fetch to use a reasonable level of parallelism.

--- git-setup.sh
+++ git-setup.sh
@@ -1,8 +1,9 @@
+git config --global credential.https://github.com.username "mike.boutin@gmail.com"
 git config --global fetch.prune true
 git config --global pretty.changelog "format:%C(auto)%h%d %Cgreen%an %Cred(%cr) %Creset%s"
 git config --global push.default simple
 git config --global push.recurseSubmodules check
-git config --global credential.https://github.com.username "mike.boutin@gmail.com"
+git config --global submodule.fetchJobs 0
 
 git config --global alias.ci "commit --verbose"
 git config --global alias.ds "diff --staged"
