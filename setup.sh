fb2f2519afa30c936039e28b1e0c66c1047d5db3
Mike Boutin <mike.boutin@gmail.com>
2016-11-21 11:18:54 -0500

    Add VSCode settings.json.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/sh
+# Bash setup.
 cp bash_profile ~/.bash_profile
 cp bashrc ~/.bashrc
 
+# Setup tmux, VsVim, mintty, and VSCode.
 if [ ! -z $MSYSTEM ]; then
     dos2unix -n tmux.conf ~/.tmux.conf
     cp _vsvimrc ~/_vsvimrc
     cp minttyrc ~/.minttyrc
+    cat settings.json | envsubst > ~/AppData/Roaming/Code/User/settings.json
 else
     cp tmux.conf ~/.tmux.conf
 fi
 
+# Source bash_profile.
 . ~/.bash_profile
 
+# Git setup.
 ./git-setup.sh
 
 [ ! -d ~/.vim/bundle/Vundle.vim ] && \
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# VIM setup and install plug-ins.
 ./vim-setup.sh
