935dfe86d1d2c3fe1e77e84cdaf85542c24ea6bb
Mike Boutin <mike.boutin@gmail.com>
2017-01-03 15:23:23 -0500

    Use XDG base directory for git and mintty.

--- setup.sh
+++ setup.sh
@@ -3,11 +3,14 @@
 cp bash_profile ~/.bash_profile
 cp bashrc ~/.bashrc
 
+mkdir -p ~/.config/git
+mkdir -p ~/.config/mintty
+
 # Setup tmux, VsVim, mintty, and VSCode.
 if [ ! -z $MSYSTEM ]; then
     dos2unix -n tmux.conf ~/.tmux.conf
     cp _vsvimrc ~/_vsvimrc
-    cp minttyrc ~/.minttyrc
+    cp minttyrc ~/.config/mintty/config
 
     [ -d "$HOME/AppData/Roaming/Code/User" ] && \
         cat settings.json | envsubst > ~/AppData/Roaming/Code/User/settings.json
