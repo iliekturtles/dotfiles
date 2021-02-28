c72d0a821e54ce23dabd21a4b6c9b7eab146f9ee
Mike Boutin <mike.boutin@gmail.com>
2021-02-28 13:27:09 -0500

    Correct nvim configuration issues.Correct nvim configuration issues.

--- vim/setup.sh
+++ vim/setup.sh
@@ -36,12 +36,13 @@ fi
 if hash nvim 2>/dev/null; then
     echo "Setup neovim config files..."
 
+    mkdir -p "$XDG_CONFIG_HOME/nvim"
     mkdir -p "$XDG_DATA_HOME/nvim/site/autoload"
     mkdir -p "$XDG_DATA_HOME/nvim/"{backup,swap,undo}
 
     cp *.vim "$XDG_CONFIG_HOME/nvim/"
 
-    if [ ! -f "$XDG_CONFIG_HOME/vim/autoload/plug.vim" ]; then
+    if [ ! -f "$XDG_DATA_HOME/nvim/site/autoload/plug.vim" ]; then
         echo "Install vim-plug..."
 
         cp "$XDG_CONFIG_HOME/vim/autoload/plug.vim" "$XDG_DATA_HOME/nvim/site/autoload/"
@@ -51,6 +52,7 @@ if hash nvim 2>/dev/null; then
     if [ ! -z "$MSYSTEM" ] && [ -d "$LOCALAPPDATA/Programs/Microsoft VS Code/bin/" ]; then
         cp win32yank.exe "$LOCALAPPDATA/Programs/Microsoft VS Code/bin/"
     elif [ ! -z "$WSL_DISTRO_NAME" ]; then
+        mkdir -p "~/.local/bin"
         cp win32yank.exe ~/.local/bin/
     fi
 
