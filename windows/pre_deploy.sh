e2680613710e78eef453e79bb57cb1f54a478145
Mike Boutin <mike.boutin@gmail.com>
2024-01-05 14:07:56 -0500

    dotter/bash setup.

--- windows/pre_deploy.sh
+++ windows/pre_deploy.sh
@@ -17,3 +17,11 @@ setenv XDG_CACHE_HOME "$(cygpath -m "{{shell.XDG_CACHE_HOME}}")"
 setenv XDG_CONFIG_HOME "$(cygpath -m "{{shell.XDG_CONFIG_HOME}}")"
 setenv XDG_DATA_HOME "$(cygpath -m "{{shell.XDG_DATA_HOME}}")"
 setenv XDG_STATE_HOME "$(cygpath -m "{{shell.XDG_STATE_HOME}}")"
+{{#if dotter.packages.bash}}
+
+echo Setting bash variables...
+setenv HISTCONTROL "{{bash.HISTCONTROL}}"
+setenv HISTFILE "{{bash.HISTFILE}}"
+setenv HISTFILESIZE "{{bash.HISTFILESIZE}}"
+setenv HISTSIZE "{{bash.HISTSIZE}}"
+{{/if}}
