8aceed9d61c24694d7cfe160f03321d94e13cd7d
Mike Boutin <mike.boutin@gmail.com>
2024-01-27 21:45:52 -0500

    dotter/sshd setup.

--- aconfmgr/dotupdate.sh
+++ aconfmgr/dotupdate.sh
@@ -30,6 +30,13 @@ if ! systemctl is-enabled fstrim.timer > /dev/null; then
     sudo systemctl enable fstrim.timer
 fi
 {{/if}}
+{{#if dotter.packages.sshd}}
+
+if ! systemctl is-enabled sshd.service > /dev/null; then
+    sudo systemctl enable sshd.service
+    sudo systemctl start sshd.service
+fi
+{{/if}}
 {{#if dotter.packages.systemd-networkd}}
 
 if ! systemctl is-enabled systemd-networkd.service > /dev/null; then
